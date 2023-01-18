3262d0be9b6231af4eee6fa08c8853663d9b51ca
Yusuf Emre Açılan <100679013+yusufemreACILAN@users.noreply.github.com>
2023-01-18 17:42:59 +0300

    Update bookAppointment.php

--- appointment/bookAppointment.php
+++ appointment/bookAppointment.php
@@ -1,40 +1,40 @@
 <?php
 
-// Available days
-$days = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+// // Available days
+// $days = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
 
-// Available hours
-$hours = array("9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00");
+// // Available hours
+// $hours = array("9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00");
 
-// Booked appointments
-$booked = array();
+// // Booked appointments
+// $booked = array();
 
-// Function to book an appointment
-function bookAppointment($day, $hour) {
-    global $days, $hours, $booked;
+// // Function to book an appointment
+// function bookAppointment($day, $hour) {
+//     global $days, $hours, $booked;
 
-    // Check if the day and hour are available
-    if (in_array($day, $days) && in_array($hour, $hours)) {
-        // Check if the appointment is not already booked
-        if (!in_array(array($day, $hour), $booked)) {
-            // Book the appointment
-            $booked[] = array($day, $hour);
-            // Remove the hour from the available hours
-            $hours = array_diff($hours, array($hour));
-            echo "Appointment booked successfully at $hour on $day \n";
-        } else {
-            echo "Sorry, the appointment at $hour on $day is already booked \n";
-        }
-    } else {
-        echo "Invalid day or hour \n";
-    }
-}
+//     // Check if the day and hour are available
+//     if (in_array($day, $days) && in_array($hour, $hours)) {
+//         // Check if the appointment is not already booked
+//         if (!in_array(array($day, $hour), $booked)) {
+//             // Book the appointment
+//             $booked[] = array($day, $hour);
+//             // Remove the hour from the available hours
+//             $hours = array_diff($hours, array($hour));
+//             echo "Appointment booked successfully at $hour on $day \n";
+//         } else {
+//             echo "Sorry, the appointment at $hour on $day is already booked \n";
+//         }
+//     } else {
+//         echo "Invalid day or hour \n";
+//     }
+// }
 
-bookAppointment("Monday", "9:00");
-bookAppointment("Monday", "9:00");
-bookAppointment("Tuesday", "9:00");
-bookAppointment("Wednesday", "10:00");
-bookAppointment("Invalid", "Invalid");
+// bookAppointment("Monday", "9:00");
+// bookAppointment("Monday", "9:00");
+// bookAppointment("Tuesday", "9:00");
+// bookAppointment("Wednesday", "10:00");
+// bookAppointment("Invalid", "Invalid");
 
 
 
@@ -45,20 +45,19 @@ bookAppointment("Invalid", "Invalid");
 $days = array("pazartesi", "sali", "carsamba", "persembe", "cuma");
 $start_time = "09:00:00";
 $end_time = "20:00:00";
-
-function bookAppointment($day, $hour){
-    global $days, $start_time, $end_time, $booked;
-    
-}
-
-
-
-
 //alınan randevular
 $booked = array();
 
+function bookAppointment($day, $hour){
+    global $days, $start_time, $end_time, $booked;
 
-
+    if (in_array($day, $days) && in_array($hour, $hours) ) { //gün ve saatlere uygun olan randevu varmı diye kontrol sağlanacak olan yer
+        
+    }
+    foreach ($days as $value) {
+        echo "$value <br>";
+    }
+}
 
 
 ?>
